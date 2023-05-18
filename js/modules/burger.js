4c91ed85b077bd3a6447b773c8a4dfdf38169f16
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-05-18 16:43:23 -0500

    add service img and prices

---
+++ js/modules/burger.js
@@ -0,0 +1,21 @@
+const d = document
+
+export function appendBurgerModal($header, $clone) {
+	const $close = d.createElement('li')
+	$close.textContent = 'Cerrar'
+	$close.classList.add('close-burger-modal')
+
+	$clone.classList.add('hidden')
+	$clone.appendChild($close)
+	$clone.classList.add('modal', 'burger-modal')
+	$header.appendChild($clone)
+}
+
+export function toggleModal(clicked, $modal) {
+	console.log($modal)
+	if (clicked.matches('.burger-menu') || clicked.matches('.burger-menu *')) {
+		$modal.classList.remove('hidden')
+	} else if (clicked.matches('.close-burger-modal')) {
+		$modal.classList.add('hidden')
+	}
+}
